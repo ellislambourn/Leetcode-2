6979aa947fab0ce4af3300fa8d40b6af7aa4b253
Ellis <minibroel@gmail.com>
2026-07-17 13:29:08 +0100

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordDict = [word for word in wordDict if word in s]
+        # the recursuve solution will TLE.
+        # can create a cache that says dp[i] is able to reach the end. 
+        dp = [None] * (len(s) + 1)
+        dp[len(s)] = True
+
+        def dfs(i):
+            if dp[i]:
+                return True
+            if dp[i] == False:
+                return False
+            if i == len(s):
+                return True
+            
+            possible = wordDict.copy()
+            for word in possible:
+                if s[i:i+len(word)] == word:
+                    if dfs(i+len(word)):
+                        dp[i] = True
+                        return True
+            dp[i] = False
+            return False
+                
+        return dfs(0)
